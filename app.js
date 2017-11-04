113c695c255905623c61a5688b2622dd08c520f1
Raphael Long <rafilong42@gmail.com>
2017-11-04 01:26:59 +0000

    Add heroku port config

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+var port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.sendFile(__dirname + "/index.html"))
-app.get('/login', (req, res) => res.sendFile(__dirname + "/login.html"))
+app.get('/', (req, res) => res.sendFile(__dirname + "/index.html"));
+app.get('/login', (req, res) => res.sendFile(__dirname + "/login.html"));
 
-app.listen(3000, () => console.log('Hello worldo'))
+app.listen(port, () => console.log('Server started'));
