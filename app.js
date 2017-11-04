ca43c58f097cf7385b2e5e166697aab49219f36c
Raphael Long <rafilong42@gmail.com>
2017-11-04 01:13:10 +0000

    Implement basic home and login page

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hey boi'))
+app.get('/', (req, res) => res.sendFile(__dirname + "/index.html"))
+app.get('/login', (req, res) => res.sendFile(__dirname + "/login.html"))
 
 app.listen(3000, () => console.log('Hello worldo'))
